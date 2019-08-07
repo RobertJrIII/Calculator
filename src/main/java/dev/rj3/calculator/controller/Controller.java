30548739cb97e810b95a606ed007d80917240b5a
RobertJrIII <roberto.gongalez02@gmail.com>
2019-08-07 13:53:53 -0700

    cleaned up code

--- src/main/java/dev/rj3/calculator/controller/Controller.java
+++ src/main/java/dev/rj3/calculator/controller/Controller.java
@@ -22,16 +22,11 @@ public class Controller {
     private GridPane pad;
     private final String NAN = "Not a number";
     private String operator = "";
-    private double num1 = 0;
 
+    private final double DEFUALT_VAL = 0;
+    private double num1 = DEFUALT_VAL;
     private Scene scene;
-    @FXML
-    private AnchorPane root;
-    private Alert alert;
 
-    public void initialize() {
-
-    }
 
     @FXML
     private void operatorClicked(MouseEvent e) {
@@ -51,10 +46,11 @@ public class Controller {
             if (!operator.isEmpty()) return;
 
             operator = value;
+
             try {
-                num1 = Double.parseDouble(output.getText());
+                num1 = Double.parseDouble(getOutputText());
             } catch (NumberFormatException e) {
-                output.setText(NAN);
+                setOutputText(NAN);
 
                 return;
             }
@@ -65,26 +61,29 @@ public class Controller {
             if (operator.isEmpty()) return;
             double num2;
             try {
-                num2 = Double.parseDouble(output.getText());
+                num2 = Double.parseDouble(getOutputText());
             } catch (NumberFormatException e) {
-                output.setText(NAN);
+                setOutputText(NAN);
                 return;
             }
 
             double answer = round(Calculate.calculate(num1, num2, operator));
 
-            String answerStr;
-
-            if ((answer == Math.floor(answer)) && !Double.isInfinite(answer)) {
-                answerStr = String.valueOf((int) answer);
-            } else {
-                answerStr = String.valueOf(answer);
-            }
-
-
-            output.setText(answerStr);
+            setOutputText(displayAnswer(answer));
             operator = "";
         }
+    }
+
+    private String displayAnswer(double answer) {
+        if (isInteger(answer)) {
+            return String.valueOf((int) answer);
+        } else {
+            return String.valueOf(answer);
+        }
+    }
+
+    private boolean isInteger(double answer) {
+        return (answer == Math.floor(answer)) && !Double.isInfinite(answer);
     }
 
     private double round(double answer) {
@@ -118,8 +117,8 @@ public class Controller {
 
     @FXML
     private void clearLabel() {
-        output.setText("0");
-        num1 = 0;
+        setOutputText(String.valueOf(DEFUALT_VAL));
+        num1 = DEFUALT_VAL;
         operator = "";
     }
 
@@ -137,23 +136,17 @@ public class Controller {
 
                 KeyCode keyCode = e.getCode();
                 String key = e.getText();
-
-
-                if (key.equals("%") || key.equals("+") || key.equals("-") || key.equals("*") || key.equals("/") || key.equals("=") || keyCode.equals(KeyCode.ENTER)) {
-                    if (keyCode.equals(KeyCode.ENTER)) {
-                        key = "=";
-                    }
+                if (keyCode.equals(KeyCode.ENTER)) {
+                    key = "=";
+                }
+                if (isOperator(key)) {
                     processOperator(key);
-                } else if (keyCode.equals(KeyCode.DELETE) || key.equals("C") || key.equals("c")) {
+                } else if (keyCode.equals(KeyCode.DELETE)) {
                     clearLabel();
                 } else if (keyCode.isDigitKey() || key.equals(".")) {
                     processValue(key);
                 } else if (keyCode.equals(KeyCode.BACK_SPACE)) {
-                    String val = output.getText().substring(0, output.getText().length() - 1);
-                    if (val.equals("")) {
-                        val = String.valueOf(0);
-                    }
-                    output.setText(val);
+                    delete();
                 }
 
             });
@@ -161,6 +154,22 @@ public class Controller {
 
         }
 
+    }
+
+    private void delete() {
+        String val = getOutputText().substring(0, output.getText().length() - 1);
+        if (val.equals("")) {
+            val = String.valueOf(0);
+        }
+        setOutputText(val);
+    }
+
+    private boolean isOperator(String key) {
+        return key.equals("%") || key.equals("+") || key.equals("-") || key.equals("*") || key.equals("/") || key.equals("=");
+    }
+
+    private String getOutputText() {
+        return output.getText();
     }
 
     private void setOutputText(String val) {
